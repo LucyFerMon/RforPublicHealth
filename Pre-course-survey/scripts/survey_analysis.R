f6c71c89c016a4d59a41c30785c0765f95e443ea
Lucía Fernández Montoya <LucyFerMon@users.noreply.github.com>
2017-02-08 18:24:07 +0100

    Delete survey_analysis.R

--- Pre-course-survey/scripts/survey_analysis.R
+++
@@ -1,9 +0,0 @@
-
-
-#### Reading survey results and scoring participants
-
-survey_results <- read_csv("~/RforPublicHealth/Pre-course survey/data/R pre-training survey.csv",
-                           col_names = c("date", "username", "name", "r_use", "r_before","tools", "programming", "equation", "histogram", "var_types", "categorical","median") )
-
-
-#### Score 
